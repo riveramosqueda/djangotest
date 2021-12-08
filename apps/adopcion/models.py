023b5573a0c3801d4f953c0bf51a9c8dbf2a10aa
Juan Rivera <juan.rivera@codebay.us>
2021-12-08 14:34:52 -0600

    Mascota and Adopcion apps.py files have been updated name and first migrations have been created

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+  nombre=models.CharField(max_length=50)
+  apellidos=models.CharField(max_length=50)
+  edad=models.IntegerField()
+  telefono=models.CharField(max_length=20)
+  email=models.EmailField(max_length=50)
+  domicilio=models.TextField()
--- apps/mascota/apps.py
+++ apps/mascota/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MascotaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mascota'
+    name = 'apps.mascota'

ab5686540894f58f9181001501fc8c17278854bf
Juan Rivera <juan.rivera@codebay.us>
2021-12-14 14:02:00 -0600

    Added Vacuna model class. Added relations to Mascota model: Vacuna and Persona. Added admin CRUDs for Adopcion, Mascota and Vacuna

--- apps/mascota/admin.py
+++ apps/mascota/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from apps.mascota.models import Mascota, Vacuna
 
 # Register your models here.
+admin.site.register(Mascota)
+admin.site.register(Vacuna)
--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from apps.adopcion.models import Persona
+
+class Vacuna(models.Model):
+  nombre=models.CharField(max_length=50)
 
 # Create your models here.
 class Mascota(models.Model):
@@ -6,3 +10,6 @@ class Mascota(models.Model):
   sexo=models.CharField(max_length=10)
   edad_aproximada=models.IntegerField()
   fecha_rescate=models.DateField()
+  # relations
+  persona=models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
+  vacuna=models.ManyToManyField(Vacuna, blank=True)
